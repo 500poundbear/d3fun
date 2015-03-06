97da6ba3438fdbd1066a0df2263bdc9acdce95fe
Bear <namjjd@gmail.com>
2015-03-06 15:09:59 +0800

    Conway

---
+++ conway.js
@@ -0,0 +1,24 @@
+var screenh = 800;
+var screenw = 600;
+
+var Screen = function(obj){
+	this.height=screenh;
+	this.width=screenw;
+}
+Screen.prototype.draw=function(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
